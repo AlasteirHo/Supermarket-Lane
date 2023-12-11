605209f92d6dfa21d92d19cdc12cff46622e2a59
iTrqzy <amanmiah@outlook.com>
2023-12-11 11:56:55 +0000

    Changes for 11th December

--- 271123/CashierLanes.py
+++ 271123/CashierLanes.py
@@ -2,7 +2,7 @@ from CheckoutClass import Checkout
 import json
 import random
 
-#A  random number between 1 and 10 customers need to join the checkout lane at the beginning of the simulation.
+#A random number between 1 and 10 customers need to join the checkout lane at the beginning of the simulation.
 #self.LaneMax = 25
 #for self service theres 1 lane of 8 tills = 7 people can be queuing at one time if all 8 tills are open and has customers
 # Customers made -> Lane1 -> Is Lane1 Full? -> Open new lane ELSE Put customer in Lane1 -> Process Customer -> Output lane status? -> Remove Customer from lane
@@ -14,29 +14,20 @@ class Lane(Checkout):
         super().__init__()
 
     def extract_ordered_customers(self):
-        with open("StoringData/OrderedCustomers.json", "r") as f:
+        with open("StoringData/Cashier.json", "r") as f:
             data = json.load(f)
         return data
 
-    # def get_data(self):
-    #     a = self.extract_ordered_customers()
-    #     print(a)
 
     def create_first_lane(self):
         customers = self.extract_ordered_customers()
         customer_keys = list(customers.keys())
-        # print(random.sample(customer_keys,9))
-
-
-    def add_lane_to_dict(self):
-        pass
-
+        print(random.sample(customer_keys, 9))
 
 
 
 b = Lane()
 b.create_first_lane()
-print("Testing")
 
 
 
