7bc54fd58e9c4391501aea51bfcad6c3f4652fed
iTrqzy <amanmiah@outlook.com>
2023-12-19 20:42:01 +0000

    Changes made

--- 271123/CheckoutClass.py
+++ 271123/CheckoutClass.py
@@ -3,40 +3,57 @@ import time
 from datetime import datetime
 import random
 
+#MAX Cashier Lane = 5
 
 class Checkout:
     def __init__(self):
         self.LaneStatus = "Open"
-        self.Lane = {}
+        self.CashierLane = {}
+        self.SelfCheckout = {}
         self.ordered_dict_items = {}
 
-
-    def CreateCashierLane(self):
-    #     for i in range(1,6):
-            TestVal = random.randint(1,6)
-            # LaneNumber = f"Manned Tills {i}"
+    #Functions that deal with the creation of lanes:
+    def CreateCashierLane(self,LaneNumber):
             self.NewLane = {
                 "TimeStamp": self.getTime(),
                 "LaneOpen": self.LaneStatus,
-                "CustomersInLane": TestVal
+                "CustomersInLane": 0,
             }
-            self.Lane["Cashier Lane 1"] = self.NewLane
-            self.WriteLaneDict()
+            self.CashierLane[LaneNumber] = self.NewLane
+            self.WriteCashierLane()
 
+    def WriteCashierLane(self):
+        with open("StoringData/CashierLane.json", "w") as f:
+            f.write(json.dumps(self.CashierLane,indent=2))
 
     def CreateSelfCheckoutLane(self):
-        self.Lane.update({
+        self.SelfCheckout.update({
             f"SelfCheckoutTill {i}": {
                 "LaneOpen": "Open",
                 "CustomersInSelfCheckoutLane": 0,
             } for i in range(1,9)
         })
-        self.WriteLaneDict()
+        self.WriteSelfCheckoutLane()
 
+    def WriteSelfCheckoutLane(self):
+        with open("StoringData/SelfCheckout.json", "w") as f:
+            f.write(json.dumps(self.SelfCheckout,indent=2))
 
-    def WriteLaneDict(self):
-        with open("StoringData/Lanes.json", "w") as f:
-            f.write(json.dumps(self.Lane,indent=2))
+#########################################################################################################################
+
+    #Extracts the cashier lanes.
+    def ExtractCashierLanes(self):
+        with open("StoringData/CashierLane.json", "r") as f:
+            data = json.load(f)
+        return data
+
+    # if ordered_data[keys]["Basket Size"] >= 10:
+    def CheckCashierLane(self):
+    #Needs to check if the current lane open/given to the function is full or not.
+        data = self.ExtractCashierLanes()
+        if len(data) != 5:
+            for items in data:
+                if data[items]["LaneOpen"] == True and data[items]["CustomerInLane"] == 0: #Complete this
 
     def ExtractCustomerData(self):
         with open("StoringData/customer_data.json", "r") as f: #Change this to the new file
@@ -70,8 +87,9 @@ Checkout1 = Checkout()
 # Checkout1.ExtractCustomerData()
 # Checkout1.SortCustomer()
 
-Checkout1.CreateCashierLane()
+Checkout1.CreateCashierLane(1)
 Checkout1.CreateSelfCheckoutLane()
+Checkout1.CheckCashierLane()
 
 
 
