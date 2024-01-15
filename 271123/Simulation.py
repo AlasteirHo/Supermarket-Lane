2dd063a1db701fbed588021a6ee4fbef8698d4f5
AlasteirHo <150919468+AlasteirHo@users.noreply.github.com>
2024-01-15 12:48:55 +0000

    Add files via upload

--- 271123/Simulation.py
+++ 271123/Simulation.py
@@ -1,15 +1,16 @@
 import random
 import time
 from datetime import datetime, timedelta
+
 from CustomerItemProcessor import Customer, ItemProcessing
-from SelfCheckoutLanes import SelfCheckout
-from Lane import Lanes
-from CashierLanes import CashierLanes
+# from SelfCheckoutLanes import SelfCheckout
+# from Lane import Lanes
+# from CashierLanes import CashierLanes
 
 
 class Simulation(Customer):
-    def __init__(self, basket_size):
-        super().__init__(basket_size)
+    def __init__(self):
+        super().__init__()
         self.simulation_time = None
         self.opening_time = datetime.strptime('07:00:00', '%H:%M:%S')
         self.closing_time = datetime.strptime('23:00:00', '%H:%M:%S')
@@ -30,23 +31,18 @@ class Simulation(Customer):
 
     def initialize_simulation(self):
         self.simulation_time = self.opening_time
-        print(f"Simulation started at {self.simulation_time.strftime('%H:%M')}")
+        print(f"Simulation started at {self.simulation_time.strftime('%H:%M:%S')}")
         self.initialize_customers()
-        self.simulation_time += timedelta(minutes=30)
+        self.simulation_time += timedelta(seconds=30)
 
     def main_loop(self):
         simulation_interval = 5
         end_simulation = False
 
         while self.simulation_time.time() < self.closing_time.time() and not end_simulation:
-            user_input = input("Enter anything to end the simulation or press enter to continue to next interval: ")
-            if user_input:
-                end_simulation = True
-            else:
-                simulation_time = self.simulation_time
-                print(f"Current Time: {simulation_time.strftime('%H:%M')}")
+            simulation_time = self.simulation_time
+            print(f"Current Time: {simulation_time.strftime('%H:%M:%S')}")
 
-            # Generate a random number of customers (up to 10) every 30 seconds
             num_customers = random.randint(1, 10)
             for i in range(num_customers):
                 customer_spawn = Customer.create_customer()
@@ -62,16 +58,13 @@ class Simulation(Customer):
                 Customer.save_customer_dict_to_json()
 
             # Move time forward in the simulation
-            self.simulation_time += timedelta(minutes=30)
+            self.simulation_time += timedelta(seconds=30)
 
             # Simulate real-time passage (sleep for 30 seconds)
             time.sleep(simulation_interval)
 
-        # Display the final state of the customer dictionary
-        # Customer.display_customer_dict()
-
 
 if __name__ == "__main__":
-    simulation = Simulation(0)
+    simulation = Simulation()
     simulation.initialize_simulation()
     simulation.main_loop()
