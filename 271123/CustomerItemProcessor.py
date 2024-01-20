d90eeccd70e776fcb66045aaac90a1cf656507ed
AlasteirHo <150919468+AlasteirHo@users.noreply.github.com>
2024-01-20 15:58:55 +0000

    Add files via upload

--- 271123/CustomerItemProcessor.py
+++ 271123/CustomerItemProcessor.py
@@ -30,12 +30,14 @@ class ItemProcessing:
         # Determines if the customer wins a lottery ticket based on basket size
         try:  # Error handling to ensure that basket_size is an integer between 1 and 30
             lottery_status = "hard luck, better luck next time!"
+            lottery_message = ""
             basket_size = int(customer.basket_size)
             if 10 < basket_size <= 30:
                 customer.lottery_ticket = random.choice([True, False])
                 if customer.lottery_ticket:
                     lottery_status = "Lucky Winner!"
-            return lottery_status
+                    lottery_message = "### Lucky customer ###"
+            return lottery_status, lottery_message
         except ValueError as e:
             print(f"Error awarding customer a lottery ticket: {e} ")
             return "Lottery status unable to assign due to invalid basket size"
@@ -44,15 +46,11 @@ class ItemProcessing:
     def display_customer_details(customer_dict):
         # Display details of each customer, including lottery status and processing times in the specified format
         for customer_id, customer in customer_dict.items():
-            lottery_status = ItemProcessing.award_lottery(customer)
-            if lottery_status == "wins a lottery ticket!":
-                lucky_customer_message = "### Lucky customer ###"
-            else:
-                lucky_customer_message = ""
+            lottery_status,lottery_message = ItemProcessing.award_lottery(customer)
 
             customer_key = "C" + str(customer.customer_id)
             print(
-                f"{lucky_customer_message}\n"
+                f"{lottery_message}\n"
                 f"{customer_key} —> items in basket: {customer.basket_size}, "
                 f"{lottery_status}, \n"
                 f"Time to process basket at cashier till: {customer.processing_time_cashier} Secs, \n"
@@ -114,7 +112,6 @@ class Customer(ItemProcessing):
                 f"time_at_cashier: {customer.processing_time_cashier}, "
                 f"time_at_self_service: {customer.processing_time_self_checkout}\n"
             )
-
 
 # Uncomment the following lines to test the code:
 # C1 = Customer()
