f1d8ca8e7f89e1b487a446f2e22b36d2b4236d7e
AlasteirHo <150919468+AlasteirHo@users.noreply.github.com>
2024-01-22 14:21:31 +0000

    Add files via upload

--- 271123/Run.py
+++ 271123/Run.py
@@ -1,6 +1,13 @@
 import time
 import random
 from CustomerItemProcessor import Customer
+from Lane import Lanes
+from CashierLanes import CashierLanes
+from SelfCheckoutLanes import SelfCheckout
+
+lane = Lanes()
+cashier_lanes = CashierLanes()
+self_checkout = SelfCheckout()
 
 
 class Simulation:
@@ -9,21 +16,44 @@ class Simulation:
         self.start_time = 0
 
     def initialize_simulation(self):
-        start_time = time.time()  # Save the start time
+        start_time = time.time()  # Save the start time to be used to print elapsed time at the end
         start_time_str = time.strftime("%H:%M:%S", time.localtime(start_time))
-        print(f"Simulation started at: {start_time_str}")
-
-        initial_customer = random.randint(1, 10)
+        print(f"Simulation started at: {start_time_str}")  # Prints start time at the beginning
+        initial_customer = random.randint(1, 10)  # Creates a random number of customers between 1 and 10
         for i in range(initial_customer):
             C1 = Customer()
             C1.create_customer()
         self.is_running = True
 
-    def main_loop(self):
+    @staticmethod
+    def initialize_lanes():
+        # Initializes the lanes that are needed by default.
+        lane.create_lane("cashier")
+        lane.create_lane("self_checkout")
+        # Creates the files needed.
+        self_checkout.create_self_checkout_file()
+        cashier_lanes.create_cashier_file()
+
+    def update_lane(self):
+        self.is_running = True
+        if self.is_running:
+            # Starts the simulation by printing a message and initialize the lanes
+            self.initialize_lanes()
+            self_checkout_total = lane.self_checkout_customer_total()
+            cashier_total = lane.cashier_customer_total()
+            total_customers = self_checkout_total + cashier_total  # Will check if all lanes are full or not.
+            if total_customers == 40:
+                print("Lane saturation")  # Will print Lane saturation and avoid the next iterations.
+            else:
+                self_checkout.main()
+                cashier_lanes.main()
+
+    def main_loop(self):  # The main loop to create customers and sort them into their respective lanes
         self.customer_creator()
+        lane.sort_customer()
 
     @staticmethod
-    def customer_creator():
+    def customer_creator():  # Creates a customer when called on
         customer = Customer()
         customer.create_customer()
 
@@ -45,4 +75,6 @@ if __name__ == "__main__":
         interval = random.randint(1, 5)
         S1.main_loop()
         S1.display_cus_details()
+        S1.update_lane()
+        lane.display_lane_status()
         time.sleep(interval)
